40da79fd250573e15d680da9cb42c8d9071fb2d2
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2025-10-28 11:19:36 +0100

    update indentation

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
         nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
         nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
         firefox-addons = {
-        url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-        inputs.nixpkgs.follows = "nixpkgs";
+            url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
@@ -17,27 +17,27 @@
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             inherit system;
             modules = [
-            ./configuration.nix
-            nix-flatpak.nixosModules.nix-flatpak
-            home-manager.nixosModules.home-manager
-            {
-                nixpkgs.config.allowUnfree = true;
-                nixpkgs.overlays = [
-                    nix-vscode-extensions.overlays.default
-                    firefox-addons.overlays.default
-                ];
-            }
-            {
-                home-manager.backupFileExtension = "backup";
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.pierre = {
-                imports = [
-                    nix-flatpak.homeManagerModules.nix-flatpak
-                    ./home/pierre/home.nix
-                ];
-                };
-            }
+                ./configuration.nix
+                nix-flatpak.nixosModules.nix-flatpak
+                home-manager.nixosModules.home-manager
+                {
+                    nixpkgs.config.allowUnfree = true;
+                    nixpkgs.overlays = [
+                        nix-vscode-extensions.overlays.default
+                        firefox-addons.overlays.default
+                    ];
+                }
+                {
+                    home-manager.backupFileExtension = "backup";
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+                    home-manager.users.pierre = {
+                        imports = [
+                            nix-flatpak.homeManagerModules.nix-flatpak
+                            ./home/pierre/home.nix
+                        ];
+                    };
+                }
             ];
         };
     };
