8790a24ec759bd39490acb93d67f051703f36fc8
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2026-02-05 11:22:33 +0100

    home: remove unnecesary inputs;  install bat, fzf and add a please alias to bash;

--- home/pierre/shell.nix
+++ home/pierre/shell.nix
@@ -3,10 +3,32 @@
         ./starship.nix
     ];
 
+
+    # Kinda like 'cat' but with code/config highlighting
+    programs.bat.enable = true;
+
+    # Fuzzy finder. Used for viewing history (ctrl+r) and searching files (ctrl+t) inside terminal.
+    programs.fzf = {
+      enable = true;
+      enableBashIntegration = true;
+
+      defaultOptions = [
+        "--height 40%"
+        "--border"
+      ];
+
+      fileWidgetOptions = [
+        # Preview files with 'bat'
+        "--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
+      ];
+    };
+
     programs.bash = {
-        enable = true;
-            shellAliases = {
-            lla = "ll -a";
-        };
+      enable = true;
+      # Bash aliases
+      shellAliases = {
+        lla = "ll -a";
+        please = "sudo !!";
+      };
     };
 }
