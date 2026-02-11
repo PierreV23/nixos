7fb99f52c205a82821908ddbfdd8cf005b5cf25a
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2026-02-11 13:54:56 +0100

    move localization to seperate nix file

--- configuration.nix
+++ configuration.nix
@@ -7,9 +7,6 @@
 {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-
-
-
     imports =
         [ # Include the results of the hardware scan.
         ./hardware-configuration.nix
@@ -21,26 +18,6 @@
     # Bootloader.
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-
-
-
-    # Set your time zone.
-    time.timeZone = "Europe/Amsterdam";
-
-    # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
-
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "nl_NL.UTF-8";
-        LC_IDENTIFICATION = "nl_NL.UTF-8";
-        LC_MEASUREMENT = "nl_NL.UTF-8";
-        LC_MONETARY = "nl_NL.UTF-8";
-        LC_NAME = "nl_NL.UTF-8";
-        LC_NUMERIC = "nl_NL.UTF-8";
-        LC_PAPER = "nl_NL.UTF-8";
-        LC_TELEPHONE = "nl_NL.UTF-8";
-        LC_TIME = "nl_NL.UTF-8";
-    };
 
     # Enable the X11 windowing system.
     services.xserver.enable = true;
--- system/default.nix
+++ system/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./flatpak.nix
+    ./locale.nix
     ./location.nix
     ./networking.nix
     ./virtualization.nix
