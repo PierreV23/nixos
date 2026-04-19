7fbdcf702ecb6ce2648d4c8f65bd1bfc9e22ac4c
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2026-04-19 12:40:22 +0000

    nova: refactor

--- hosts/nova/system/users.nix
+++ hosts/nova/system/users.nix
@@ -1,45 +1,5 @@
 { secrets, pkgs, ... }:
 {
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
-    settings.PermitRootLogin = "prohibit-password";
-    extraConfig = ''
-      Match User media
-        ChrootDirectory /mnt/data/media
-        ForceCommand internal-sftp
-        AllowTcpForwarding no
-        X11Forwarding no
-      Match User pierre
-        ChrootDirectory /mnt/data/pierre
-        ForceCommand internal-sftp
-        AllowTcpForwarding no
-        X11Forwarding no
-    '';
-  };
-
-  services.fail2ban = {
-    enable = true;
-    maxretry = 3;
-    bantime = "24h";
-    bantime-increment = {
-      enable = true;
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h";
-      overalljails = true;
-    };
-    ignoreIP = [
-      "10.0.0.0/8"
-      "192.168.0.0/16"
-      "172.16.0.0/12"
-      secrets.home_ip
-    ];
-  };
-
-  services.openssh.settings = {
-    MaxStartups = "10:30:100";
-  };
-
   users.users.root = {
     hashedPassword = secrets.nova.root_password;
     openssh.authorizedKeys.keys = [
