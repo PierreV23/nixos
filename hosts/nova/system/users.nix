bf1201752acb7930fea1bd3131132145aa358537
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2026-04-19 13:37:59 +0200

    nova: implement fake2ban

--- hosts/nova/system/users.nix
+++ hosts/nova/system/users.nix
@@ -18,6 +18,28 @@
     '';
   };
 
+  services.fail2ban = {
+    enable = true;
+    maxretry = 3;
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
+    };
+    ignoreIP = [
+      "10.0.0.0/8"
+      "192.168.0.0/16"
+      "172.16.0.0/12"
+      secrets.home_ip
+    ];
+  };
+
+  services.openssh.settings = {
+    MaxStartups = "10:30:100";
+  };
+
   users.users.root = {
     hashedPassword = secrets.nova.root_password;
     openssh.authorizedKeys.keys = [
